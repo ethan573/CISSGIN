e390ca2d9f890ab4c914d9534bff2cd216d81afe
tsangaaron09 <aaron2018008@concordiashanghai.org>
2017-06-23 16:12:09 +0800

    CISSMUN main page and GINASIA main page

--- app/src/main/java/com/example/fatboy/cissgin/HomeScreen.java
+++ app/src/main/java/com/example/fatboy/cissgin/HomeScreen.java
@@ -1,9 +1,11 @@
 package com.example.fatboy.cissgin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.graphics.drawable.*;
 import android.graphics.Color;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -12,5 +14,19 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         getSupportActionBar().hide();
+    }
+    public void onButtonClick(View v)
+    {
+        if(v.getId() == R.id.Bcissmun)
+        {
+            Intent i = new Intent(HomeScreen.this,cissmunmain.class);
+            startActivity(i);
+        }
+        else if (v.getId() == R.id.Bginasia)
+        {
+            Intent i = new Intent(HomeScreen.this,ginasiamain.class);
+            startActivity(i);
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/fatboy/cissgin/cissmunmain.java
@@ -0,0 +1,17 @@
+package com.example.fatboy.cissgin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Fatboy on 22/6/2017.
+ */
+
+public class cissmunmain extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cissmunmain);
+
+    }
+}
