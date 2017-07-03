62dfdba618752abcec0900b88ad99139f615d8ca
ethanwang <ethan573@gmail.com>
2017-07-02 23:43:50 -0500

    bottombar v1

--- app/src/main/java/com/example/fatboy/cissgin/cissmunmain.java
+++ app/src/main/java/com/example/fatboy/cissgin/cissmunmain.java
@@ -1,17 +1,62 @@
 package com.example.fatboy.cissgin;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-/**
- * Created by Fatboy on 22/6/2017.
- */
+public class cissmunmain extends AppCompatActivity {
 
-public class cissmunmain extends Activity
-{
+    private TextView mTextMessage;
+
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                case R.id.navigation_map:
+
+                case R.id.navigation_procedure:
+
+                case R.id.navigation_schedule:
+
+                case R.id.navigation_shanghai:
+
+
+
+
+
+            }
+            return true;
+        }
+
+    };
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
         setContentView(R.layout.cissmunmain);
 
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_home);
+
+
     }
+
 }
+
